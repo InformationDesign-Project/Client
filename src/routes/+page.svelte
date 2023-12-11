d693fcd961406abb4046c1cb2eb8258767a8e8ad
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-12-11 20:08:22 +0900

    Add : 메인페이지 퍼블리싱 중, chartjs, d3js, Highcharts 설치

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-    // TypeScript 로직 추가
-  </script>
+    </script>
   
   <body>
     <div class="header">
@@ -27,8 +26,53 @@
     </div>
   
     <div class="dashboard">
-      <!-- 대시보드 내용 -->
-    </div>
+        <div class="dashboard-top-line">
+            <div class="dashboard-coin">COSMOS</div>
+            <div class="dashboard-metric">
+              <div class="metric-value">85</div> <!-- 임의의 값 -->
+              <div class="metric-label">Healthy Level</div>
+            </div>
+            <div class="dashboard-metric">
+              <div class="metric-value score">92</div> <!-- 임의의 값 -->
+              <div class="metric-label">Healthy Score</div>
+            </div>
+            <div class="dashboard-metric">
+              <div class="metric-value">76%</div> <!-- 임의의 값 -->
+              <div class="metric-label">Decentralization</div>
+            </div>
+          </div>
+        <div class="dashboard-main">
+            <div class="heatmap"></div>
+            <div class="sidebar">
+                <div class="sorted-by">Sorted By</div>
+                <div class="validators-info">
+                    <div class="validators-info-item">
+                      <div class="validators-info-label">Total Validators</div>
+                      <div class="validators-info-number">50</div>
+                    </div>
+                    <div class="validators-info-item">
+                      <div class="validators-info-label">Level 1</div>
+                      <div class="validators-info-number">10</div>
+                    </div>
+                    <div class="validators-info-item">
+                      <div class="validators-info-label">Level 2</div>
+                      <div class="validators-info-number">15</div>
+                    </div>
+                    <div class="validators-info-item">
+                      <div class="validators-info-label">Level 3</div>
+                      <div class="validators-info-number">20</div>
+                    </div>
+                    <div class="validators-info-item">
+                      <div class="validators-info-label">Level 4</div>
+                      <div class="validators-info-number">12</div>
+                    </div>
+                    <div class="validators-info-item">
+                      <div class="validators-info-label">Level 5</div>
+                      <div class="validators-info-number">8</div>
+                    </div>
+                  </div>
+        </div>
+      </div>
   
     <div class="coin-list-section">
       <!-- 코인 목록 섹션 -->
@@ -77,9 +121,11 @@
 }
 
 .coin-name {
+    
   font-size: 34px; 
-  font-weight: normal; 
+  font-weight: bold; 
   margin-bottom: 5px; 
+  
 }
 
 .title {
@@ -87,13 +133,110 @@
   font-weight: normal; 
   opacity: 0.6;
 }
+.dashboard-coin{
+  background-color: #161B26; 
+  padding: 15px 15px; 
+  border-radius: 10px; 
+  width: 150px; 
+  height: 50px; 
 
+  
+  font-size: 24px; 
+  color: #FFFFFF; 
+  
+}
+
+.sorted-by{
+    font-size: 12px;
+    font-style: normal;
+    color: #7987A8;
+}
 
 
   
-    .dashboard {
-      // 대시보드 스타일링
+.dashboard {
+  display: flex;
+  width: 900px; 
+  height: 500px; 
+  padding: 40px; 
+  flex-direction: column; 
+  align-items: flex-start; 
+  gap: 30px; 
+  flex-shrink: 0; 
+  background-color: #11141B; 
+  color:#fff;
+  border-radius: 10px; 
+  margin: 20px; 
+
+  .dashboard-top-line {
+    display: flex;
+    justify-content: space-between;
+    width: 85%; 
+    .dashboard-metric {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .metric-value {
+      font-size: 50px; 
+      font-weight: bold; 
     }
+    .metric-value.score {
+  color: #267AF9; 
+}
+
+    .metric-label {
+      font-size: 16px; 
+      margin-top: 5px; 
+      opacity: 0.6;
+    }
+  }
+}
+
+  .dashboard-main {
+    display: flex;
+    width: 100%; 
+
+
+
+    .sidebar {
+      flex-basis: 200px;
+      margin-left:30px;
+    }
+  }
+}
+
+.validators-info-item {
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+}
+
+/* validators-info-label 스타일 */
+.validators-info-label {
+  font-size: 12px;
+  font-weight: normal;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  color: #7987A8;
+}
+
+.validators-info-number {
+  font-size: 24px;
+  font-weight: bold;
+  color: #FFFFFF; 
+}
+
+.heatmap{
+    display: flex;
+    width: 900px; 
+    height: 400px; 
+    background-color: #161B26; 
+    border-radius: 10px;
+
+}
+
+
   
     .coin-list-section {
       // 코인 목록 섹션 스타일링
