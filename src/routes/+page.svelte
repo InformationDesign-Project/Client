8710bdd3311dfa06f04ed6454e3cc0f9d7e2fadc
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-12-11 17:02:49 +0900

    Add : 메인 페이지 레이아웃 작성 중

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,102 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    // TypeScript 로직 추가
+  </script>
+  
+  <body>
+    <div class="header">
+      <div class="logo">
+        <img width="70%;" src="/layout/Healthy-chain.png" alt="Healthy Chain Logo"/>
+      </div>
+      <div class="top-coins">
+        <div class="top-coin today">
+          <div class="time-period">Today</div>
+          <div class="coin-name"><!-- 오늘의 최고 건강도 코인 이름 -->COSMOS</div>
+          <div class="title">Best Healthy Chain Today</div>
+        </div>
+        <div class="top-coin month">
+          <div class="time-period">Month</div>
+          <div class="coin-name"><!-- 이번 달의 최고 건강도 코인 이름 -->COSMOS</div>
+          <div class="title">Best Healthy Chain this Month</div>
+        </div>
+        <div class="top-coin year">
+          <div class="time-period">Year</div>
+          <div class="coin-name"><!-- 올해의 최고 건강도 코인 이름 -->COSMOS</div>
+          <div class="title">Best Healthy Chain this Year</div>
+        </div>
+      </div>
+    </div>
+  
+    <div class="dashboard">
+      <!-- 대시보드 내용 -->
+    </div>
+  
+    <div class="coin-list-section">
+      <!-- 코인 목록 섹션 -->
+    </div>
+  </body>
+  
+  <style lang="scss">
+    .header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  
+    .logo {
+      // 로고 스타일링
+      flex-grow: 0;
+      padding-left: 40px; 
+      padding-top: 15px;  
+    }
+  
+    .top-coins {
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  padding-top: 15px;  
+}
+
+.top-coin {
+  background-color: #11141B;
+  color: #FFFFFF; 
+  padding: 28px; 
+  border-radius: 10px; 
+  margin: 0 15px; 
+  width: 150px; 
+  height: 50px; 
+  display: flex;
+  flex-direction: column; 
+  align-items: flex-start; 
+}
+
+.time-period {
+  font-size: 16px; 
+  font-weight: normal; 
+  margin-bottom: 5px; 
+  
+}
+
+.coin-name {
+  font-size: 34px; 
+  font-weight: normal; 
+  margin-bottom: 5px; 
+}
+
+.title {
+  font-size: 11px; 
+  font-weight: normal; 
+  opacity: 0.6;
+}
+
+
+
+  
+    .dashboard {
+      // 대시보드 스타일링
+    }
+  
+    .coin-list-section {
+      // 코인 목록 섹션 스타일링
+    }
+  </style>
+  
