f8dab5f4e2526d3223b2ebff8940a743b2f26c3b
tmddv <105033488+tmddv@users.noreply.github.com>
2023-12-18 06:33:58 +0900

    Feat:메인 테이블

---
+++ src/components/Main/NetworkTable.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { afterUpdate } from "svelte";
+
+    export let chainData;
+    export let changeName;
+    afterUpdate(()=>{
+        console.log(chainData,'내 컴포넌트')
+    })
+</script>
+
+<main>
+    <div>
+        {#each chainData as data}
+					<div on:click={()=>changeName(data.chain)} style='color:orange;'>
+                        {data.chain}
+                    </div>
+				{/each}
+    </div>
+    
+</main>
+<style>
+    
+</style>
